71d94caf26a658ae51ba703d09ea7fae73b7b7eb
zulfiihzam <35595962+zulfiihzam@users.noreply.github.com>
2020-10-30 20:03:33 +0700

    Add files via upload

---
+++ IF1-10117125-Latihan18-Kelipatan/src/if1/pkg10117125/latihan18/kelipatan/IF110117125Latihan18Kelipatan.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10117125.latihan18.kelipatan;
+
+/**
+ *
+ * @author 
+ * NAMA : Zulfi Ihzam Rahmat
+ * KELAS: IF-1
+ * NIM  : 10117125
+ * Deskripsi Program : Program ini berisi program perulangan yang menampilkan 
+ * kelipatan angka 3.5 secara ascending
+ */
+public class IF110117125Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (double i = 3.5; i <= 35; i += 3.5){
+            System.out.println(i);
+        }
+    }
+    
+}
